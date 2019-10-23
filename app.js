d367dbb421808bf81d98d73515957540f3253f81
Donald Gjeloshi <dongje11@htl-shkoder.com>
2019-10-23 18:49:46 +0200

    add a welcome page, send the the client with chunks --> with method write()

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
-const http = require("http");
+const { Server } = require("http");
 
 /* create a http server */
-function requestCallback(request, response) {
-  response.end("Hello Node.js");
-}
+const server = new Server();
 
-const server = http.createServer(requestCallback);
+/* Register a callback for request events */
+server.on("request", (request, response) => {
+  response.writeHead(200, { "Content-Type": "text/html" });
+  response.write("<html>");
+  response.write("<head>");
+  response.write("<title> My First Page</title>");
+  response.write("</head>");
+  response.write("<body>");
+  response.write("<h1>Hello there</h1>");
+  response.write("</body>");
+  response.write("</html>");
+  response.end();
+});
 
-/* Bind server to a port */
-server.listen(8080);
+/* Bind server to a port and hostname*/
+const port = 8080;
+const host = "127.0.0.1";
+server.listen(port, host, () => {
+  console.log(`Server running at http://${host}:${port}/`);
+});
