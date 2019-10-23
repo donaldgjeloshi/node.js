a878edd06590d7e564828a580a46c2ccfa6283a7
Donald Gjeloshi <dongje11@htl-shkoder.com>
2019-10-23 19:20:13 +0200

    routing with statusCode 404

--- app.js
+++ app.js
@@ -3,18 +3,26 @@ const { Server } = require("http");
 /* create a http server */
 const server = new Server();
 
+const welcomePage = `<html>
+ <head><title>My First Page</title></head>
+ <body><h1>Hello from my Node.js Server!</h1></body>
+ </html>`;
+const defaultPage = `<html>
+ <head><title>My First Page</title></head>
+ <body>
+ <h1>This Page is not available!</h1>
+ <h2>Take me <a href="/index.html">home</a>.</h2>
+ </body>
+ </html>`;
+
 /* Register a callback for request events */
 server.on("request", (request, response) => {
-  response.writeHead(200, { "Content-Type": "text/html" });
-  response.write("<html>");
-  response.write("<head>");
-  response.write("<title> My First Page</title>");
-  response.write("</head>");
-  response.write("<body>");
-  response.write("<h1>Hello there</h1>");
-  response.write("</body>");
-  response.write("</html>");
-  response.end();
+  response.setHeader("Content-Type", "text/html");
+  if (request.url === "/index.html") {
+    return response.end(welcomePage);
+  }
+  response.statusCode = 404;
+  response.end(defaultPage);
 });
 
 /* Bind server to a port and hostname*/
